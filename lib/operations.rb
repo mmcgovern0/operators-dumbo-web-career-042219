97125823385fd81841cc10922a7f189ece94d093
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-09 20:29:39 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,14 @@
 def unsafe?(speed)
+  if speed > 60
+    true
+  en
 
 end
 
 
 
 def not_safe?(speed)
-	  if speed > 60 && speed < 40 ? true : false
+	  if speed > 60 || speed < 40 ? true : false
   end
 end
 	
